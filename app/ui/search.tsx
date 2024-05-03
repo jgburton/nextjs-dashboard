c918298008bca2445ca98d3e0e0862094906aecd
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2024-05-03 19:18:09 +1000

    implemented search and pagination using URL Params and Next.js APIs

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     term ? params.set('query', term) : params.delete('query');
     replace(`${pathname}?${params.toString()}`);
   }, 300);
